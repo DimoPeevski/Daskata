10894ac649a419212918966a31c079f8907bdc26
Amen Ruby <didiprophet@gmail.com>
2024-03-18 20:01:58 +0200

    database fixes

--- Daskata/Data/DaskataDbContext.cs
+++ Daskata/Data/DaskataDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Configuration;
 
 namespace Daskata.Data
 {
@@ -9,5 +10,6 @@ namespace Daskata.Data
             : base(options)
         {
         }
+
     }
 }
